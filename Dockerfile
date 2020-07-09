595e01e772fd55b9ba9e87c23f0a7e81a35d2c41
ade <adedoja@everlytic.com>
2020-07-09 11:23:24 +0200

    ready to tet

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ RUN apt update && \
     apt install -y python3-dev gcc
 
 WORKDIR app 
+
 # Install pytorch and fastai
 #RUN pip install torch_nightly -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html
 
@@ -14,9 +15,9 @@ ADD models models
 ADD src src
 
 # Run it once to trigger resnet download
-RUN python src/app.py prepare
+#RUN python src/app.py prepare
 
 #EXPOSE 5000
-
+COPY . app
 # Start the server
-CMD ["python", "src/app.py", "serve"]
+CMD ["python", "app.py", "serve"]
--- src/app.py
+++ src/app.py
@@ -7,16 +7,27 @@ from io import BytesIO
 from typing import List, Dict, Union, ByteString, Any
 import os
 import flask
-from flask import Flask
+from flask import Flask, flash, request, redirect, url_for, session, jsonify
 import requests
 import torch
 import json
+
+from werkzeug.utils import secure_filename
+from flask_cors import CORS, cross_origin
+
 
 with open("src/config.yaml", 'r') as stream:
     APP_CONFIG = yaml.full_load(stream)
 
 app = Flask(__name__)
 export_file_name = 'export.pkl'
+UPLOAD_FOLDER = '/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def load_model(path=".", model_name="model.pkl"):
     learn = load_learner(path, export_file_name)
@@ -50,6 +61,34 @@ def predict(img, n: int = 3) -> Dict[str, Union[str, List]]:
     predictions = predictions[0:n]
     return {"class": str(pred_class), "predictions": predictions}
 
+def classify_file(url: str):
+    img = open_image(url)
+    res = predict(img)
+    return res
+
+
+@app.route('/file-upload', methods=['POST'])
+def upload_image_file():
+	# check if the post request has the file part
+	if 'file' not in request.files:
+		resp = jsonify({'message' : 'No file part in the request'})
+		resp.status_code = 400
+		return resp
+	file = request.files['file']
+	if file.filename == '':
+		resp = jsonify({'message' : 'No file selected for uploading'})
+		resp.status_code = 400
+		return resp
+	if file and allowed_file(file.filename):
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		resp = jsonify({'message' : classify_file(app.config['UPLOAD_FOLDER'] + filename)})
+		resp.status_code = 201
+		return resp
+	else:
+		resp = jsonify({'message' : 'Allowed file types are png, jpg, jpeg, gif'})
+		resp.status_code = 400
+		return resp
 
 @app.route('/api/classify', methods=['POST', 'GET'])
 def upload_file():
@@ -69,9 +108,28 @@ def classes():
     return flask.jsonify(classes)
 
 
-@app.route('/ping', methods=['GET'])
+# @app.route('/ping', methods=['GET'])
+# def ping():
+#     return "pong"
+
+@app.route('/ping', methods=['GET', 'POST'])
 def ping():
-    return "pong"
+    target=os.path.join(UPLOAD_FOLDER,'test_docs')
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    file = request.files['file'] 
+    filename = secure_filename(file.filename)
+    destination="/".join([target, filename])
+    file.save(destination)
+    session['uploadFilePath']=destination
+    response="Whatever you wish too return"
+    return response
+
+    
+
+@app.route('/api/upload', methods=['GET'])
+def classify():
+    return "classify"
 
 
 @app.route('/config')
@@ -114,5 +172,5 @@ if __name__ == '__main__':
     if "prepare" not in sys.argv:
         app.jinja_env.auto_reload = True
         app.config['TEMPLATES_AUTO_RELOAD'] = True
-        app.run(debug=False, host='0.0.0.0', port=port)
+        app.run(debug=True, host='0.0.0.0', port=port)
         # app.run(host='0.0.0.0', port=port)
